c012a4d5c38e38b409f216430ee3e600ed64f4b2
Eduardo Watanabe <eduardowatanabelima@gmail.com>
2024-07-15 13:20:28 -0300

    ajustando nginx

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::post("register", [AuthController::class, "register"]);
 Route::post("login", [AuthController::class, "login"]);
 Route::post("update-password/{id}", [AuthController::class, "resetFromBot"]);
-Route::get("/", [TestController::class, "index"]);
+Route::get("/test", [TestController::class, "index"]);
 
 Route::middleware("auth:sanctum")->group(function() {
     Route::get("/posts/{id}", [PostController::class, "show"]);
